63ffc70902a113ec8febffd4a00a5bc9a31647bd
mojtaba khalilfar <mojtabakhalilfar11@gmail.com>
2025-05-03 13:01:40 +0330

    Portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,9 +1,24 @@
+import Headline from '@/component/Headline'
+import Link from 'next/link'
 import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div id='portfolio' className='h-[100vh]'>
-      Portfolio
+    <div id='portfolio' className='flex items-center justify-center py-10'>
+      <div className='flex flex-col items-center justify-center'>
+        <div className=''>
+          <Headline headline='PORTFOLIO' />
+        </div>
+        <div className='flex flex-col items-center sm:grid grid-cols-3 gap-0 py-8'>
+          {
+            [1, 2, 3, 4, 5, 6].map((items) => (
+              <Link href={"#"}>
+                <img className='w-[382px] h-[209px]' src="" alt="" />
+              </Link>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
